6ec1e5c53d49dceef0ad85ff3bdea5aef795fc50
rkubica <rkubica@edokumenty.eu>
2020-03-24 22:58:33 +0100

    Add Exception class InvalidPropertyException Add model class for Validation

---
+++ app/Model/WhiteList/InvalidPropertyException.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Model\WhiteList;
+
+use Exception;
+
+/**
+ * Class InvalidPropertyException
+ * @package App\Model\WhiteList
+ * @author              Robert Kubica <rkubica@edokumenty.eu>
+ * @copyright       (c) eDokumenty Sp. z o.o.
+ */
+class InvalidPropertyException extends Exception {
+    
+}
