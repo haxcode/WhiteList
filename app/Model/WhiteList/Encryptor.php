d4888cd123a26ad3e4aa088ce78986a3e9cd1ca1
rkubica <rkubica@edokumenty.eu>
2020-09-16 23:30:59 +0200

    Add class to mit with expectation from TDD tests

--- app/Model/WhiteList/Encryptor.php
+++ app/Model/WhiteList/Encryptor.php
@@ -3,19 +3,51 @@
 
 namespace App\Model\WhiteList;
 
-
+/**
+ * Class Encryptor
+ * @package App\Model\WhiteList
+ * @author Robert Kubica <rkubica@edokumenty.eu>
+ * @copyright (c) eDokumenty Sp. z o.o.
+ */
 class Encryptor {
-    public static function encryptSHA($toHash, $iterations = 1) {
-        for($i = 0; $i < $iterations; $i++) {
+    
+    /**
+     * @param string $date
+     * @param string $nip
+     * @param string $nrb
+     * @param int $iterations
+     * @return string
+     */
+    public static function encrypt(string $date, string $nip, string $nrb = '', $iterations = 5000): string {
+        return self::encryptSHA($date.$nip.$nrb, $iterations);
+    }
+    
+    /**
+     * @param string $toHash
+     * @param int $iterations
+     * @return string
+     */
+    public static function encryptSHA(string $toHash, int $iterations = 5000): string {
+        for ($i = 0; $i < $iterations; $i++) {
             $toHash = hash('sha512', $toHash);
         }
+        
         return $toHash;
     }
-
-    public static function encrypt($date, $nip, $nrb, $iterations){
-        return self::encryptSHA($date.$nip.$nrb, $iterations);
+    
+    /**
+     * @param string $mask
+     * @param string $nrb
+     * @return string
+     */
+    public static function maskCombine(string $mask, string $nrb) {
+        $mask = str_split($mask);
+        $nrb = str_split($nrb);
+        $output = '';
+        foreach ($mask as $index => $element) {
+            $output .= ($element == 'Y') ? $nrb[$index] : $element;
+        }
+        return $output;
     }
 }
 
-
-?>
